4db08eac0219fc06c4cbb24b6d8e7618a4255d09
Panagiotis Efstathiou <pefstathiou@outlook.com>
2023-05-13 12:09:36 +0300

    Creates the models

---
+++ src/main/java/com/edu/babymonitoringappj2ee/model/Account.java
@@ -0,0 +1,14 @@
+package com.edu.babymonitoringappj2ee.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Account {
+
+    private int id;
+    private String name;
+}
---
+++ src/main/java/com/edu/babymonitoringappj2ee/model/Record.java
@@ -0,0 +1,17 @@
+package com.edu.babymonitoringappj2ee.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Record {
+    private int id;
+    private double duration;
+    private Date date;
+    private double quantity;
+}
