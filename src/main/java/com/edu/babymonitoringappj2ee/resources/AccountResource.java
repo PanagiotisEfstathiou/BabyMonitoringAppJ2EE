426b47e1485e0219364e8718b8a22a404c371c1a
PanagiotisEfstathiou <pefstathiou@outlook.com>
2023-05-14 23:21:48 +0300

    Creates Resources and servlets

---
+++ src/main/java/com/edu/babymonitoringappj2ee/resources/AccountResource.java
@@ -0,0 +1,15 @@
+package com.edu.babymonitoringappj2ee.resources;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+
+@Path("/account")
+public class AccountResource {
+    @GET
+    @Path("/ping")
+    @Produces("text/plain")
+    public String hello() {
+        return "Hello, World!";
+    }
+}
---
+++ src/main/java/com/edu/babymonitoringappj2ee/servlets/AccountServlet.java
@@ -0,0 +1,7 @@
+package com.edu.babymonitoringappj2ee.servlets;
+
+import javax.servlet.annotation.WebServlet;
+
+@WebServlet(name = "AccountServlet", value = "/accountServlet")
+public class AccountServlet {
+}
